a24f24662ba4448db6ce0d8348f4e29de6bbcfa3
d-abiyoga <dimas.abiyoga169@gmail.com>
2021-08-02 14:02:47 +0700

    update app.js using promises

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const fetchPokemon = () => {
     console.log(`fetching PokeMon`);
+    
+    const promises = [];
     for (let i = 1; i <= 150; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        fetch(url)
-            .then( res => {
-                return res.json();
-            })
-            .then( data => {
-                // console.log(data);
-                
-                const pokemon = {
-                    name: data.name,
-                    id: data.id,
-                    image: data.sprites['front_default'],
-                    type: data.types
-                        .map( type => type.type.name)
-                        .join(", ")
-                };
-            
-                console.log(pokemon);
-            });
+        promises.push(fetch(url).then( res => res.json()));
     }
+
+    Promise.all(promises).then( results => {
+        const pokemon = results.map(data => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types
+                .map( type => type.type.name)
+                .join(", ")       
+        }))
+        displayPokemon(pokemon);
+        // console.log(pokemon);
+    });      
+
+}
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
 }
 
 fetchPokemon();
