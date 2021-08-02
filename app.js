d3ea48398b48ac4dd9ef5ad995086575ce000fb9
d-abiyoga <dimas.abiyoga169@gmail.com>
2021-08-02 13:38:09 +0700

    update app.js to fetch 150 pokemon

---
+++ app.js
@@ -0,0 +1,26 @@
+const fetchPokemon = () => {
+    console.log(`fetching PokeMon`);
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        fetch(url)
+            .then( res => {
+                return res.json();
+            })
+            .then( data => {
+                // console.log(data);
+                
+                const pokemon = {
+                    name: data.name,
+                    id: data.id,
+                    image: data.sprites['front_default'],
+                    type: data.types
+                        .map( type => type.type.name)
+                        .join(", ")
+                };
+            
+                console.log(pokemon);
+            });
+    }
+}
+
+fetchPokemon();
